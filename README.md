b108588349e2178d107e5d36aac62b35268bd8a5
Lucas Grigolon Varela <lucasgrvarela@gmail.com>
2023-06-03 18:26:44 -0300

    feat: improving the code and the readme

--- README.md
+++ README.md
@@ -1,8 +1,52 @@
-The result will be similar to that:
-```
-Result of function ScanPort (scan specific port): 1024
-Port result: {tcp/1024 Closed}
+# Port Scanner
 
-Result of function InitialScan (scan all ports from 1 to 1024):
-[{tcp/1 Closed} {udp/1 Open} {tcp/2 Closed} {udp/2 Open} ... {tcp/1024 Closed} {udp/1024 Open}]
+The Port Scanner is a command-line tool that allows you to scan ports on a target host. It can determine whether a specific port is open or closed by establishing a connection to it.
+
+## Usage
+
+To use the Port Scanner tool, follow the steps below:
+
+1. Clone the repository:
+```bash
+git clone https://github.com/lucasgrvarela/port-scanner.git
 ```
+
+2. Change into the project directory:
+```bash
+cd port-scanner
+```
+
+3. Build the executable:
+```bash
+go build -o port-scanner
+```
+
+4. Run the scanner:
+```bash
+./port-scanner [protocol] [hostname] [port]
+```
+- [protocol]: The protocol to use (e.g., tcp or udp).
+- [hostname]: The hostname or IP address of the target.
+- [port]: The port number to scan. Specify 0 to scan all ports from 1 to 1024.
+
+## Example
+1. Scan one specific port
+```bash
+./port-scanner tcp localhost 80
+{tcp/80 Open}
+```
+
+2. Scan all ports from 1 to 1024
+```bash
+./port-scanner tcp localhost 0
+Port result: {tcp/1 Closed}
+Port result: {tcp/2 Closed}
+Port result: {tcp/3 Closed}
+...
+Port result: {tcp/80 Open}
+Port result: {tcp/81 Closed}
+...
+```
+
+# Contributing
+Contributions to this project are welcome! If you have any suggestions, bug reports, or want to add new features, feel free to open an issue or submit a pull request.
--- main.go
+++ main.go
@@ -1,11 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+)
 
 func main() {
-	result := ScanPort("tcp", "localhost", 1024)
-	fmt.Printf("Port result: %v\n", result)
+	if len(os.Args) != 4 {
+		fmt.Println("Usage: go run main.go <protocol> <hostname> <port>")
+		fmt.Println("If port == 0 then it will scan all ports from 1 to 1024, if any other number is specified it will scan only this specific port")
+		return
+	}
 
-	results := InitialScan("localhost")
-	fmt.Println(results)
+	protocol := os.Args[1]
+	if protocol != "udp" && protocol != "tcp" {
+		log.Fatal("Invalid protocol, you can use 'udp' or 'tcp'")
+	}
+
+	hostname := os.Args[2]
+	port, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		log.Fatal("Invalid port number")
+	}
+
+	if port == 0 {
+		results := ScanAll(hostname)
+		for _, v := range results {
+			fmt.Println(v)
+		}
+		return
+	}
+	fmt.Printf("%v\n", ScanOnePort(protocol, hostname, port))
+}
+
+type ScanResult struct {
+	Port  string
+	State string
+}
+
+func ScanOnePort(protocol string, hostname string, port int) ScanResult {
+	result := ScanResult{Port: fmt.Sprintf("%s/%d", protocol, port)}
+	address := fmt.Sprintf("%s:%d", hostname, port)
+	conn, err := net.Dial(protocol, address)
+	if err != nil {
+		result.State = "Closed"
+		return result
+	}
+	defer conn.Close()
+
+	result.State = "Open"
+	return result
+}
+
+func ScanAll(hostname string) []ScanResult {
+	var results []ScanResult
+
+	for port := 1; port <= 1024; port++ {
+		result := ScanOnePort("tcp", hostname, port)
+		results = append(results, result)
+	}
+
+	return results
 }
